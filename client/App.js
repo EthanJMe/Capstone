0dbf2c2ff9c2785d4efd9c5fc354688e76b21f90
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-02-11 01:34:37 -0800

    created home page base UI and notes

--- client/App.js
+++ client/App.js
@@ -1,12 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
-import Headers from "./navigation/Headers";
 import PageStack from './navigation/PageStack';
  function App() {
 
   return (
 <>
-
-
     <PageStack/>
   </>
   );
--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
+import { Text, Button, StyleSheet, TextInput, } from 'react-native';
+  /*1. create state for each field
+    2. create function to do math
+    3. display result of math function below calculate button
+    */
 function Home({navigation}) {
   const AccountTypesHandler = () => {
     navigation.navigate('AccountTypes')
@@ -11,9 +15,35 @@ function Home({navigation}) {
   <>
   <Button title='Account Types' onPress={AccountTypesHandler}/>
   <Button title='Fees And Charges' onPress={FeesAndChargesHandler}/>
-  <Text>hi</Text>
+  <Text style = {styles.Header}>Welcome to our interest calculator</Text>
+  <Text style ={styles.SmallHeader}>Initial Deposit</Text>
+  <TextInput style={styles.input}/>
+  <Text style ={styles.SmallHeader}>Interest Rate (APY)</Text>
+  <TextInput style={styles.input}/>
+  <Text style ={styles.SmallHeader}>Years in account</Text>
+  <TextInput style={styles.input}/>
+  <Button title='Calculate'/>
+
   </>
     );
+
 }
 
+const styles = StyleSheet.create({
+  Header: {
+    fontSize: 22,
+    textAlign: "center",
+  },
+  SmallHeader: {
+    fontSize: 18,
+    textAlign: "center",
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 5,
+  },
+})
 export default Home;
