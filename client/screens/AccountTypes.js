dab20ad6afc7ace49e3cb8b01301d4ef0e66d2a3
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-02-14 10:56:41 -0800

    Accordions are working, basic info added

--- client/screens/AccountTypes.js
+++ client/screens/AccountTypes.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Text, Button } from 'react-native';
+import { List } from 'react-native-paper';
+
 function AccountTypes({navigation}) {
  
   const FeesAndChargesHandler = () => {
@@ -13,7 +15,21 @@ function AccountTypes({navigation}) {
     <>
   <Button title='Fees And Charges' onPress={FeesAndChargesHandler}/>
   <Button title='Home' onPress={HomeHandler}/>
-  <Text>hi</Text>
+  <List.Accordion title="Extreme Checking">
+        <List.Item title="Earn 1.00 - 2.00% APY* on balances up to $15,000" />
+        <List.Item title="ATM fee refunds, up to $28 per month**" />
+        <List.Item title="No minimum balance required" />
+        <List.Item title="No monthly service charge" />
+        <List.Item title="No fee for not meeting criteria" />
+        <List.Item title="Free debit MasterCard® available immediately when you open your account in branches" />
+        <List.Item title="Deposit checks using your smartphone with free mobile deposit" />
+        <List.Item title="Free withdrawals at over 30,000 network ATMs" />
+        <List.Item title="Account access at 5,000 shared branches worldwide" />
+        <List.Item title="Cleared checks can be viewed online and printed" />
+        <List.Item title="Overdraft protection is available" />
+      </List.Accordion>
+      <Text>for more information go to https://www.bluefcu.com/bank/</Text>
+
   </>);
 }
 
