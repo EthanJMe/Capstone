6cd69bba0a2a78d7ef916ebe5d55ecf67d204326
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-02-16 11:34:22 -0800

    calculator works and displays

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,29 +1,66 @@
-import React from 'react';
+import { useState } from 'react';
 import { Text, Button, StyleSheet, TextInput, } from 'react-native';
+import { Alert, Modal, Pressable, View } from "react-native";
+
   /*1. create state for each field
     2. create function to do math
     3. display result of math function below calculate button
     */
 function Home({navigation}) {
+  const Calculator = (deposit, myInterest, yearsInAccount) => {
+    parseInt(deposit)
+    parseInt(myInterest)
+    parseInt(yearsInAccount)
+    const product = deposit * myInterest/100*yearsInAccount
+    return (
+      <Text>You would have {product} dollars</Text>
+    )
+  }
   const AccountTypesHandler = () => {
     navigation.navigate('AccountTypes')
   }
   const FeesAndChargesHandler = () => {
     navigation.navigate('FeesAndCharges')
   }
+
+  const [initialDeposit, setInitialDeposit] = useState('')
+  const [interest, setInterest] = useState('')
+  const [years, setYears] = useState('')
+  const [modalVisible, setModalVisible] = useState(false);
   return (
   <>
   <Button title='Account Types' onPress={AccountTypesHandler}/>
   <Button title='Fees And Charges' onPress={FeesAndChargesHandler}/>
   <Text style = {styles.Header}>Welcome to our interest calculator</Text>
   <Text style ={styles.SmallHeader}>Initial Deposit</Text>
-  <TextInput style={styles.input}/>
+  <TextInput keyboardType='numeric' style={styles.input} onChangeText={newText => setInitialDeposit(newText)}/>
   <Text style ={styles.SmallHeader}>Interest Rate (APY)</Text>
-  <TextInput style={styles.input}/>
+  <TextInput keyboardType='numeric' style={styles.input} onChangeText={newText => setInterest(newText)}/>
   <Text style ={styles.SmallHeader}>Years in account</Text>
-  <TextInput style={styles.input}/>
-  <Button title='Calculate'/>
+  <TextInput keyboardType='numeric' style={styles.input} onChangeText={newText => setYears(newText)}/>
+  <Button title='Calculate' onPress={() => setModalVisible(!modalVisible)}/>
 
+  <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text>{Calculator( deposit = initialDeposit,myInterest = interest,yearsInAccount = years)}</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
   </>
     );
 
@@ -45,5 +82,46 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 5,
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 })
 export default Home;
