75252693a5eca17aa38afb02e1ae4b04744fad6d
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-03-03 10:20:10 -0800

    adding users successfully

--- client/API/index.js
+++ client/API/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: "http://192.168.0.14:27017"})
 
 export const getUser = () => API.get('/get');
 export const addUser = (user) => API.post('/post', user);
