1454493e3033db9c276e4d1518700edaf0ae51d3
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-03-04 01:21:55 -0800

    information moved to local array

--- client/screens/AccountTypes.js
+++ client/screens/AccountTypes.js
@@ -2,7 +2,20 @@ import { Text, Button } from 'react-native';
 import { List } from 'react-native-paper';
 
 function AccountTypes({navigation}) {
- 
+ const blueFederalArray = [
+`Earn 1.00 - 2.00% APY* on balances up to $15,000,
+ATM fee refunds,
+up to $28 per month**,
+No minimum balance required,
+No monthly service charge,
+No fee for not meeting criteria,
+Free debit MasterCard® available immediately when you open your account in branches,
+Deposit checks using your smartphone with free mobile deposit
+Free withdrawals at over 30,000 network ATMs
+Account access at 5,000 shared branches worldwide
+Cleared checks can be viewed online and printed
+`]
+
   const FeesAndChargesHandler = () => {
     navigation.navigate('FeesAndCharges')
   }
@@ -13,23 +26,14 @@ function AccountTypes({navigation}) {
   const DictionaryHandler = () => {
     navigation.navigate('Dictionary')
   }
+  const extremeCheckingDescription = blueFederalArray[0]
   return (
     <>
   <Button title='Dictionary' onPress={DictionaryHandler}/>
   <Button title='Fees And Charges' onPress={FeesAndChargesHandler}/>
   <Button title='Home' onPress={HomeHandler}/>
-  <List.Accordion title="Extreme Checking">
-        <List.Item title="Earn 1.00 - 2.00% APY* on balances up to $15,000" />
-        <List.Item title="ATM fee refunds, up to $28 per month**" />
-        <List.Item title="No minimum balance required" />
-        <List.Item title="No monthly service charge" />
-        <List.Item title="No fee for not meeting criteria" />
-        <List.Item title="Free debit MasterCard® available immediately when you open your account in branches" />
-        <List.Item title="Deposit checks using your smartphone with free mobile deposit" />
-        <List.Item title="Free withdrawals at over 30,000 network ATMs" />
-        <List.Item title="Account access at 5,000 shared branches worldwide" />
-        <List.Item title="Cleared checks can be viewed online and printed" />
-        <List.Item title="Overdraft protection is available" />
+  <List.Accordion title="Blue Federal Credit Union">
+        <List.Item title="Extreme Checking" description = {extremeCheckingDescription} descriptionNumberOfLines = {20} />
       </List.Accordion>
       <Text>for more information go to https://www.bluefcu.com/bank/</Text>
 
