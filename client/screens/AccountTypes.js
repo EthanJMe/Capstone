75ec1eccd7de721c0d8a00105018d3859bf4348a
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-03-07 02:48:46 -0800

    Data json added

--- client/screens/AccountTypes.js
+++ client/screens/AccountTypes.js
@@ -2,19 +2,9 @@ import { Text, Button } from 'react-native';
 import { List } from 'react-native-paper';
 
 function AccountTypes({navigation}) {
- const blueFederalArray = [
-`Earn 1.00 - 2.00% APY* on balances up to $15,000,
-ATM fee refunds,
-up to $28 per month**,
-No minimum balance required,
-No monthly service charge,
-No fee for not meeting criteria,
-Free debit MasterCard® available immediately when you open your account in branches,
-Deposit checks using your smartphone with free mobile deposit
-Free withdrawals at over 30,000 network ATMs
-Account access at 5,000 shared branches worldwide
-Cleared checks can be viewed online and printed
-`]
+ const blueFederalArray = [{ 
+description: "Earn 1.00 - 2.00% APY* on balances up to $15,000\nATM fee refunds\nup to $28 per month**\nNo minimum balance required\nNo monthly service charge\nNo fee for not meeting criteria\nFree debit MasterCard® available immediately when you open your account in branches\nDeposit checks using your smartphone with free mobile deposit\nFree withdrawals at over 30,000 network ATMs\nAccount access at 5,000 shared branches worldwide\nCleared checks can be viewed online and printed",
+title: "Extreme Checking"},]
 
   const FeesAndChargesHandler = () => {
     navigation.navigate('FeesAndCharges')
@@ -26,14 +16,15 @@ Cleared checks can be viewed online and printed
   const DictionaryHandler = () => {
     navigation.navigate('Dictionary')
   }
-  const extremeCheckingDescription = blueFederalArray[0]
+  const extremeCheckingDescription = blueFederalArray[0].description
+  const extremeCheckingTitle = blueFederalArray[0].title
   return (
     <>
   <Button title='Dictionary' onPress={DictionaryHandler}/>
   <Button title='Fees And Charges' onPress={FeesAndChargesHandler}/>
   <Button title='Home' onPress={HomeHandler}/>
   <List.Accordion title="Blue Federal Credit Union">
-        <List.Item title="Extreme Checking" description = {extremeCheckingDescription} descriptionNumberOfLines = {20} />
+        <List.Item title={extremeCheckingTitle} description = {extremeCheckingDescription} descriptionNumberOfLines = {20} />
       </List.Accordion>
       <Text>for more information go to https://www.bluefcu.com/bank/</Text>
 
