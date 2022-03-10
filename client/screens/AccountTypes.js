538cf12e1de2900472c8c1e7fe5528d996e8bedb
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-03-10 14:31:28 -0800

    more hardcoded stuff

--- client/screens/AccountTypes.js
+++ client/screens/AccountTypes.js
@@ -5,8 +5,13 @@ import { getData } from '../API';
 
 function AccountTypes({navigation}) {
  const blueFederalArray = [{ 
-description: "Earn 1.00 - 2.00% APY* on balances up to $15,000\nATM fee refunds\nup to $28 per month**\nNo minimum balance required\nNo monthly service charge\nNo fee for not meeting criteria\nFree debit MasterCard® available immediately when you open your account in branches\nDeposit checks using your smartphone with free mobile deposit\nFree withdrawals at over 30,000 network ATMs\nAccount access at 5,000 shared branches worldwide\nCleared checks can be viewed online and printed",
-title: "Extreme Checking"},]
+  description: "Earn 1.00 - 2.00% APY* on balances up to $15,000\nATM fee refunds\nup to $28 per month**\nNo minimum balance required\nNo monthly service charge\nNo fee for not meeting criteria\nFree debit MasterCard® available immediately when you open your account in branches\nDeposit checks using your smartphone with free mobile deposit\nFree withdrawals at over 30,000 network ATMs\nAccount access at 5,000 shared branches worldwide\nCleared checks can be viewed online and printed",
+  title: "Extreme Checking"},
+{
+  description: "No minimum balance\nOver 30,000 surcharge free ATMs through CO-OP Network ATMs\nNo monthly service charge\nFree debit MasterCard, accepted at millions of locations worldwide\nFree online banking and Bill Pay",
+  title:'Basic Checking'
+}]
+
 
   const FeesAndChargesHandler = () => {
     navigation.navigate('FeesAndCharges')
@@ -20,6 +25,8 @@ title: "Extreme Checking"},]
   }
   const extremeCheckingDescription = blueFederalArray[0].description
   const extremeCheckingTitle = blueFederalArray[0].title
+  const basicCheckingDescription = blueFederalArray[1].description
+  const basicCheckingTitle = blueFederalArray[1].title
   return (
     <>
   <Button title='Dictionary' onPress={DictionaryHandler}/>
@@ -27,6 +34,7 @@ title: "Extreme Checking"},]
   <Button title='Home' onPress={HomeHandler}/>
   <List.Accordion title="Blue Federal Credit Union">
         <List.Item title={extremeCheckingTitle} description = {extremeCheckingDescription} descriptionNumberOfLines = {20} />
+        <List.Item title={basicCheckingTitle} description = {basicCheckingDescription} descriptionNumberOfLines = {20} />
       </List.Accordion>
       <Text>for more information go to https://www.bluefcu.com/bank/</Text>
 
--- client/screens/Dictionary.js
+++ client/screens/Dictionary.js
@@ -18,10 +18,8 @@ function Dictionary({navigation}) {
     <Button title='Home' onPress={HomeHandler}/>
     <Button title='Account Types' onPress={AccountTypesHandler}/>
     <Button title='Fees And Charges' onPress={FeesAndChargesHandler}/>
-    <List.Accordion title="APY">
-        <List.Item title="APY is the rate of return earned on an investment" />
-        <List.Item title = "taking into account the effect of"/>
-        <List.Item title = "compounding interest."/>
+    <List.Accordion title="Terms">
+        <List.Item title = 'APY' description = "APY is the rate of return earned on an investment taking into account the effect of compounding interest." />
       </List.Accordion>
     </>
   )
