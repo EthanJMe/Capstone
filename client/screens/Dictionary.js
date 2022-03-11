6d4bfc6aa2e5b23bd60f0e9f6de2bc40664d017c
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-03-10 19:16:05 -0800

    one more dictionary term

--- client/screens/Dictionary.js
+++ client/screens/Dictionary.js
@@ -20,6 +20,7 @@ function Dictionary({navigation}) {
     <Button title='Fees And Charges' onPress={FeesAndChargesHandler}/>
     <List.Accordion title="Terms">
         <List.Item title = 'APY' description = "APY is the rate of return earned on an investment taking into account the effect of compounding interest." />
+        <List.Item title = 'Initial Deposit' description = "An  initial deposit is the minimum amount of money required to open an account with a financial institution, such as a bank or brokerage firm. " />
       </List.Accordion>
     </>
   )
